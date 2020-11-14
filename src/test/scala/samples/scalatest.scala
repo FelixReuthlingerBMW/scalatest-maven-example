9df131604bc60beae539c14b88785b98cbca2bf5
Chua Chee Seng <cheeseng@amaseng.com>
2020-11-14 17:23:49 +0800

    Added an example of FlatSpec.

--- src/test/scala/samples/scalatest.scala
+++ src/test/scala/samples/scalatest.scala
@@ -64,3 +64,26 @@ class ExampleSpec extends AnyFunSpec {
     }
   }
 }
+
+import org.scalatest.flatspec.AnyFlatSpec
+import scala.collection.mutable.Stack
+
+class StackSpec extends AnyFlatSpec {
+
+  behavior of "A Stack"
+
+  it should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    assert(stack.pop() === 2)
+    assert(stack.pop() === 1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new Stack[String]
+    intercept[NoSuchElementException] {
+      emptyStack.pop()
+    }
+  }
+}
